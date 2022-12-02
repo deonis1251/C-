e7e3e0153724831cbd884fe2f82537cb15fdf95d
Denis <denis_gulbin@mail.ru>
2022-12-02 23:58:14 +0300

    homework1

---
+++ Homework1/Task1/Program.cs
@@ -0,0 +1,9 @@
+﻿int numberA = 2;
+int numberB = 3;
+int numberC = 7;
+int max = numberA;
+if (numberA > max) max = numberA;
+if (numberB > max) max = numberB;
+if (numberC > max) max = numberC;
+
+Console.WriteLine(max);
---
+++ Homework1/Task3/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Напишите число: ");
+int m = int.Parse(Console.ReadLine());
+for(int n = 1; n <= m; n++)
+{
+    if(n % 2 == 0) 
+    {
+        Console.WriteLine(n);
+    }
+}
