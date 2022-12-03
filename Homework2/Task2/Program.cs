47b15258ce0f589d73f3a5db511825b381d48366
Denis <denis_gulbin@mail.ru>
2022-12-03 22:12:39 +0300

    HW2Task2

---
+++ Homework2/Task2/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Напишиет цисло: ");
+int a = int.Parse(Console.ReadLine());
+int count = a.ToString().Length;
+int result = -1;
+if (count > 2)
+{
+    while(a > 999)
+    {
+        a = a / 10;
+    }
+    result = a % 10;
+    Console.WriteLine(result);
+}
+else
+{
+    Console.WriteLine("Нет третьего числа");
+}
--- ex011/Program.cs
+++ ex011/Program.cs
@@ -1,1 +1,3 @@
-﻿
+﻿int a = int.Parse(Console.ReadLine());
+int count = a.ToString().Length;
+Console.WriteLine(count[2]);
