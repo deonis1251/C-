3863639929a7ccc6d06fa882bad4c6011b2d9201
Denis <denis_gulbin@mail.ru>
2022-12-07 17:20:55 +0300

    HW2Task3

---
+++ Homework2/Task3/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите число дня недели: ");
+int day = int.Parse(Console.ReadLine());
+if (day > 0 & day < 8)
+{
+    if (day < 6)
+        {
+            Console.WriteLine("Wokr day");
+        }
+    else
+        {
+            Console.WriteLine("Day off");
+        }
+}
+else
+{
+    Console.WriteLine("Day is absent");
+}
