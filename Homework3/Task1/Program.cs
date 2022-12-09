b28bc5b124b26a3122e81bbf9de6f4800d234525
Denis <denis_gulbin@mail.ru>
2022-12-09 17:48:12 +0300

    HW3Task1

--- Homework3/Task1/Program.cs
+++ Homework3/Task1/Program.cs
@@ -1,1 +1,28 @@
-﻿Console.WriteLine("Bidaboom");
+﻿Console.WriteLine("Bведите пятизначное число: ");
+int a = int.Parse(Console.ReadLine());
+int a1 = a / 10000;
+int a5 = a % 10;
+int a2 = a / 1000 % 10;
+int a4 = a % 100 / 10;
+if (a > 9999 & a < 100000)
+{
+    if (a1 == a5)
+    {
+        if (a2 == a4)
+        {
+            Console.WriteLine("Число является полиндромом.");
+        }
+        else
+        {
+            Console.WriteLine("Число не является полиндромом.");
+        }
+    }
+    else
+    {
+        Console.WriteLine("Число не является полиндромом.");
+    }
+}
+else
+{
+    Console.WriteLine("Вы ввели не пятизначное число.");
+}
