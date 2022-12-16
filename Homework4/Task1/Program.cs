0d1250fa670841d68fe73d8d3fb81f6e1078874d
Denis <denis_gulbin@mail.ru>
2022-12-16 18:52:08 +0300

    HW4Task1

---
+++ Homework4/Task1/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите число: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Введите степень: ");
+int b = int.Parse(Console.ReadLine());
+int result = 1;
+for (int i = 1; i < b + 1; i++)
+{
+    result = result * a;
+}
+Console.WriteLine(result);
