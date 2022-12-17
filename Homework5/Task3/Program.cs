d92f5f1e7d292b740f8546ea49d5176b8eef76c2
Denis <denis_gulbin@mail.ru>
2022-12-17 23:44:53 +0300

    HW5Task3

---
+++ Homework5/Task3/Program.cs
@@ -0,0 +1,19 @@
+﻿double[] array = new double[10];
+double min = Int32.MaxValue;
+double max = Int32.MinValue;
+
+for (int i = 0; i < 10; i++)
+{
+    array[i] = Convert.ToDouble(new Random().Next(100,1000)) / 100;
+    Console.Write(array[i] + " ");
+    if (array[i] > max)
+        {
+            max = array[i];
+        }
+    if (array[i] < min)
+        {
+            min = array[i];
+        }
+}
+Console.WriteLine();
+Console.WriteLine(max - min);
