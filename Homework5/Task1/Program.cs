3d1a44429dff2cdc763d58c54c4bbf0d33d35f70
Denis <denis_gulbin@mail.ru>
2022-12-17 22:32:18 +0300

    HW5Task1

---
+++ Homework5/Task1/Program.cs
@@ -0,0 +1,13 @@
+﻿int[] array = new int[5];
+int count = 0;
+for(int i = 0; i < 5; i++)
+{
+    array[i] = new Random().Next(100, 1000);
+    Console.Write(array[i] + " ");
+    if (array[i] % 2 == 0)
+    {
+        count++;
+    }
+}
+Console.WriteLine();
+Console.WriteLine("Четных чисел: " + count);
