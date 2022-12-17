e0873378029682a4924ba943c4daf93a90028ecf
Denis <denis_gulbin@mail.ru>
2022-12-17 22:46:45 +0300

    HW5Task2

---
+++ Homework5/Task2/Program.cs
@@ -0,0 +1,13 @@
+﻿int[] array = new int[10];
+int sum = 0;
+for(int i = 0; i < 10; i++)
+{
+    array[i] = new Random().Next(-10, 10);
+    Console.Write(array[i] + " ");
+    if (i % 2 != 0)
+    {
+        sum += array[i];
+    }
+}
+Console.WriteLine();
+Console.WriteLine(sum);
