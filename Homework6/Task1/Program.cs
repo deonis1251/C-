2f05adf81fddc6c6a9df0a0d4a39ba3022bee226
Denis <denis_gulbin@mail.ru>
2022-12-23 22:22:02 +0300

    HW6Task1

---
+++ Homework6/Task1/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите количество чисел: ");
+int count = int.Parse(Console.ReadLine());
+int[] array = new int[count];
+int plus = 0;
+for (int i = 0; i < count; i++)
+{
+    Console.Write("Введите {0} -й элемент: ", i + 1);
+    array[i] = int.Parse(Console.ReadLine());
+    if (array[i] > 0)
+    {
+        plus++;
+    }
+}
+Console.WriteLine("Количество отчицательных чисел = " + plus);
