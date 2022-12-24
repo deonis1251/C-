ee0002e293acd787d8f008893bf48225f4869ea4
Denis <denis_gulbin@mail.ru>
2022-12-24 21:25:02 +0300

    HW7Task3

---
+++ Homework7/Task3/Program.cs
@@ -0,0 +1,21 @@
+﻿int[,] array = new int[3, 4];
+Random random = new Random();
+for(int i = 0; i < 3; i++ )
+{
+    for (int j = 0; j < 4; j++)
+    {
+        array[i, j] = random.Next(0, 11);
+        Console.Write(array[i, j] + " ");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine();
+for (int j = 0; j < array.GetLength(1); j++)
+{
+    double sum = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        sum += array[i, j];
+    }
+    Console.WriteLine($"{ sum / array.GetLength(0) }");
+}
