213549b29581b4d29c3d687ee6b7b41ded362404
Denis <denis_gulbin@mail.ru>
2022-12-27 13:16:23 +0300

    HW9Task2 not all

---
+++ Homework9/Task2/Program.cs
@@ -0,0 +1,41 @@
+﻿int[,] matrix = new int[5, 4];
+FillArrayRandom(matrix);
+PrintArray(matrix);
+int sum = 0;
+int sumString = 0;
+SumString(matrix);
+Console.WriteLine(sum);
+
+void FillArrayRandom(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next (1, 10);
+        }
+    }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void SumString(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            sum += matrix[i, j];    
+        }
+    }
+}
