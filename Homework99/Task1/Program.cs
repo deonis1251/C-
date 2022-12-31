d40a3bb7dff8f6f82eefd71175eb8da927f5897d
Denis <denis_gulbin@mail.ru>
2022-12-31 12:13:19 +0300

    Fixed Task1

---
+++ Homework99/Task1/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите число больше 1: ");
+int number = int.Parse(Console.ReadLine());
+Console.WriteLine(Numbers(number)); 
+
+string Numbers(int number)
+{
+    string result = string.Empty;
+    for (int i = 1; i <= number; number--)
+    {
+        result += $"{number} ";
+    }
+    return result;
+}
---
+++ Homework99/Task2/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите первое число: ");
+int firstNumber = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int secondNumber = int.Parse(Console.ReadLine());
+Console.WriteLine(SumNumbers(firstNumber, secondNumber));
+
+int SumNumbers(int a, int b)
+{
+    a = firstNumber;
+    b = secondNumber;
+    if(a > b)
+	{
+		b = firstNumber;
+		a = secondNumber;
+	}
+	return (b + a)*(b - a + 1)/2; // по формуле суммы членов арифметической прогрессии
+}
