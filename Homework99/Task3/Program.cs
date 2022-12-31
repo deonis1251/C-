6f469086b62886bbbd9731993a84f880311f06db
Denis <denis_gulbin@mail.ru>
2022-12-31 14:50:59 +0300

    HW9Task3

--- Homework99/Task3/Program.cs
+++ Homework99/Task3/Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿Console.Write("Введите первое не отрицательное число: ");
+int firstNumber = int.Parse(Console.ReadLine());
+Console.Write("Введите второе не отрицательное число: ");
+int secondNumber = int.Parse(Console.ReadLine());
+Console.WriteLine($"Функция Аккермана равна {Akkerman(firstNumber, secondNumber)}");
+
+
+int Akkerman(int a, int b)
+{
+  if (a == 0) return b + 1;
+  else if (b == 0) return Akkerman(a - 1, 1);
+  else return Akkerman(a - 1, Akkerman(a, b - 1));
+}
